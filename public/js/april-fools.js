9831048dd44b1e164cd1c286d409ad48050a1c4c
kuenzign <kuenzign@gmail.com>
2014-05-12 19:56:25 -0400

    modified:   public/font/acmicon.eot 	modified:   public/font/acmicon.svg 	modified:   public/font/acmicon.ttf 	modified:   public/font/acmicon.woff 	modified:   public/js/april-fools.js

--- public/js/april-fools.js
+++ public/js/april-fools.js
@@ -1,14 +1,21 @@
-$(function() {
-	var d = new Date;
-	if (d.getMonth() == 3 && d.getDate() ==1) {
-	    var rand = Math.floor((Math.random()*180)+1);
-	    document.writeln("<style>");
-	    document.writeln("html{");
-	    document.writeln("  -ms-transform: rotate("+rand+"deg);");
-	    document.writeln("  -webkit-transform: rotate("+rand+"deg);");
-	    document.writeln("  transform: rotate("+rand+"deg);");
-	    document.writeln("}");
-	    document.writeln("</style>");
+var d = new Date;
+if (d.getMonth() == 3 && d.getDate() ==1) {
+    var rand = Math.floor((Math.random()*180)+1);
+    document.writeln("<style>");
+    document.writeln("html{");
+    document.writeln("  -ms-transform: rotate("+rand+"deg);");
+    document.writeln("  -webkit-transform: rotate("+rand+"deg);");
+    document.writeln("  transform: rotate("+rand+"deg);");
+    document.writeln("}");
+    document.writeln("</style>");
+    var icons = document.getElementsByTagName('i');
+	for (var i = 0; i < icons.length; i++) {
+	    icons[i].className = 'ai ai-troll';
 	}
-});
-
+	var icons = document.getElementsByTagName('span');
+	for (var i = 0; i < icons.length; i++) {
+		if(icons[i].className != ''){
+	    	icons[i].className = 'ai ai-troll';
+	    }
+	}
+}
