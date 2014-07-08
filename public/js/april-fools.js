7b8d880eca33361889e43efdcb16225a55623c0e
bhoeting <brennan.hoeting@gmail.com>
2014-07-07 21:52:55 -0400

    Updated april fools to use jQuery.

--- public/js/april-fools.js
+++ public/js/april-fools.js
@@ -1,21 +1,14 @@
-var d = new Date;
-if (d.getMonth() == 3 && d.getDate() ==1) {
-    var rand = Math.floor((Math.random()*180)+1);
-    document.writeln("<style>");
-    document.writeln("html{");
-    document.writeln("  -ms-transform: rotate("+rand+"deg);");
-    document.writeln("  -webkit-transform: rotate("+rand+"deg);");
-    document.writeln("  transform: rotate("+rand+"deg);");
-    document.writeln("}");
-    document.writeln("</style>");
-    var icons = document.getElementsByTagName('i');
-	for (var i = 0; i < icons.length; i++) {
-	    icons[i].className = 'ai ai-troll';
-	}
-	var icons = document.getElementsByTagName('span');
-	for (var i = 0; i < icons.length; i++) {
-		if(icons[i].className != ''){
-	    	icons[i].className = 'ai ai-troll';
-	    }
-	}
+var date = new Date;
+
+if (date.getMonth() == 3 && date.getDate() == 1) {
+	alert('x')
+    var rand = Math.floor((Math.random() * 180) + 1);
+
+    $('.fa').addClass('ai-troll').removeClass('fa');
+
+    $('html').css({    	
+		'transform':         'rotate(' + rand + 'deg)',
+		'-ms-transform':     'rotate(' + rand + 'deg)',
+		'-webkit-transform': 'rotate(' + rand + 'deg)'
+    });
 }
