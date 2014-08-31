ee86499f3497a39457a748141a0ca4e498a57c92
bhoeting <brennan.hoeting@gmail.com>
2014-08-31 14:48:33 -0400

    Split controllers

--- app/controllers/CompetitorController.php
+++ app/controllers/CompetitorController.php
@@ -26,7 +26,11 @@ class CompetitorController extends \BaseController {
 		$this->team = $team;
 	}
 
-	public function storeTeam($competitionId)
+	/**
+	 * @param  int $competitionId
+	 * @return Response
+	 */
+	public function store($competitionId)
 	{
 		$input = Input::all();
 
@@ -46,73 +50,21 @@ class CompetitorController extends \BaseController {
 		}
 		catch (ModelNotValidException $e)
 		{
-			return Redirect::back()->withErrors($e->errors())->withInput();
+			return Redirect::back()
+				->withErrors($e->errors())->withInput();
 		}
-	}
-
-	/**
-	 * @param $competitionId
-	 * @return \Illuminate\Http\RedirectResponse
-	 */
-	public function storeCompetitor($competitionId)
-	{
-		$input = Input::all();
-
-		$input['user_id'] = Auth::id();
-
-		// If the user is signing up for a non-team based
-		// competition, then create a new "Team"
-		// where they are the only member
-		if ( ! array_key_exists('team_id', $input))
-		{
-			$team = $this->team->createAndValidate(['competition_id' => $competitionId]);
-
-			$input['team_id'] = $team->id;
-		}
-		else
-		{
-			$team = $this->team->find($input['team_id']);
-		}
-
-		if ($team->isFull())
-		{
-			return Redirect::back()->withFlashMessage('Sorry, this team is full.');
-		}
-
-		$this->competitor->createAndValidate($input);
-
-		return Redirect::back();
 	}
 
 	/**
 	 * @param  int $id
-	 * @return \Illuminate\Http\RedirectResponse|void
-	 * @throws Exception
+	 * @return Response
 	 */
-	public function deleteTeam($id)
+	public function destroy($teamId)
 	{
-		$team  = $this->team->find($id);
-
-		if ($team->canBeDeleted())
-		{
-			$team->competitors->delete();
-
-			$team->delete();
-
-			return Redirect::back();
-		}
-
-		App::abort(403);
-	}
-
-	/**
-	 * @param  int $id
-	 * @return \Illuminate\Http\RedirectResponse
-	 * @throws Exception
-	 */
-	public function deleteCompetitor($teamId)
-	{
-		$competitor = $this->competitor->where('team_id', $teamId)->where('user_id', Auth::id())->first();
+		$competitor = $this->competitor
+			->where('team_id', $teamId)
+			->where('user_id', Auth::id())
+			->first();
 
 		// If the competitor is not being deleted by its corresponding
 		// user or an admin, return a 403 error
