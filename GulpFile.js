9dfb180a9be48f326b549f40098bdfeae5887657
bhoeting <brennan.hoeting@gmail.com>
2014-09-01 12:56:04 -0400

    Add notification to Gulpfile for LESS compilation status

--- GulpFile.js
+++ GulpFile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var path = require('path');
 var less = require('gulp-less');
+var notify = require('gulp-notify');
 var autoprefixer = require('gulp-autoprefixer');
 
 var lessDir = './app/MasonACM/Assets/less/';
@@ -9,8 +10,18 @@ var cssDir = './public/css/';
 gulp.task('less', function() {
 	gulp.src(lessDir + 'styles.less')
 		.pipe(less())
+		.on('error', notify.onError({
+			title: 'Failure!',
+			icon: __dirname + '/fail.png',
+			message: 'An error occured while compiling LESS'
+		}))
 		.pipe(autoprefixer('last 10 versions'))
-		.pipe(gulp.dest(cssDir));
+		.pipe(gulp.dest(cssDir))
+		.pipe(notify({
+			title: 'Success',
+			icon: __dirname + '/success.png',
+			message: 'LESS compiled without error!'
+		}));
 });
 
 gulp.task('watch', function() {
