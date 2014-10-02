3d0287012f509ada0c1354f156b001747ea3a037
bhoeting <brennan.hoeting@gmail.com>
2014-10-02 14:42:14 -0400

    Add location to about page

--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -16,8 +16,11 @@
 	</div>
 	<div class="container">
 		<div class="text-section">
+			<h2>General Information</h2>
 			<p>The Mason ACM is part of an international organization, ACM.  ACM is an organization for computing professionals to deliver resources that advance computing as a science and a profession.</p>
 			<p>To learn more about ACM as a whole, please visit their <a href="http://www.acm.org/">website</a>.</p>	
+			<h2>Location:</h2>
+			<p>Mr. Kummer's room (B109) on Tuesdays and Thursdays from 2:15 to 3:15.</p>
 		</div>			
 	</div>
 @stop
