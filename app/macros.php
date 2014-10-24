55186410c7d7af9b59e5600fa2e9e47a126f0702
bhoeting <brennan.hoeting@gmail.com>
2014-10-24 10:41:39 -0400

    Remove unused macros

--- app/macros.php
+++ app/macros.php
@@ -1,32 +1,20 @@
 <?php
 
 /**
- * Generate an icon.
- */
-HTML::macro('Icon', function($icon, $iconType = 'fa')
-{
-	return "<i class='" . $iconType . " " . " " . $iconType . "-" . $icon . "'></i> &nbsp";
-});
-
-/**
- * Generate a link with an icon
- */
-HTML::macro('linkWithIcon', function($url = '/', $text = 'link', $icon = 'external-link', $options = [], $iconType = 'fa')
-{
-	return "<a href='" . URL::to($url) . "'" . HTML::attributes($options) . ">" . HTML::Icon($icon, $iconType) . $text . "</a>";
-});
-
-/**
  * Generate a delete form
  */
 Form::macro('delete', function($attributes = [])
 {
-
-	return Form::open($attributes)
-		    . "<button" . HTML::attributes(['class' => 'btn btn-danger']) . "><i class='fa fa-trash-o'></i>&nbsp;Delete</button>"
-		    . Form::close(); 
+	return Form::open($attributes) .
+			    "<button" . HTML::attributes(['class' => 'btn btn-danger']) . ">" .
+			    	"<i class='fa fa-trash-o'></i>&nbsp;Delete" .
+			    "</button>" .
+		   Form::close(); 
 });
 
+/**
+ * Convert a slugged string into a titled string 
+ */
 Str::macro('unslug', function($slug)
 {
 	return str_replace('-', ' ', Str::title($slug));
