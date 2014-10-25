fd06db431e9ce4399ef81acdab7b6e1dd8887bde
Brennan Hoeting <brennan.hoeting@gmail.com>
2014-10-25 14:21:33 -0400

    Improve roster js

--- public/js/roster.js
+++ public/js/roster.js
@@ -4,39 +4,52 @@ app.controller('RosterCtrl', function ($scope, $http, $filter) {
 	var orderBy = $filter('orderBy');
 
 	$scope.attendees = [];
-	$scope.attendeeQuery = null;
+	$scope.sortBy = 'id';
 	$scope.reverse = true;
-	$scope.partyId = $('#party-id').val();
-	$scope.baseUrl = '/lanparty/' + $scope.partyId + '/roster';
 	$scope.attendeeData = {};
+	$scope.attendeeQuery = null;
+	$scope.partyId = $('#party-id').val();
 
-	$http.get($scope.baseUrl).success(function (response) {
-		$scope.attendees = response;
-	});
+	$scope.getAttendees = function() {
+		$http.get('/lanparty/' + $scope.partyId + '/roster').success(function (response) {
+			$scope.attendees = response;
+			$scope.order($scope.sortBy, $scope.reverse);
+		});
+	};
+
+	$scope.getAttendees();
 
 	$scope.addAttendee = function() {
 		$scope.attendeeData._token = csrf_token;
 		$scope.attendeeData.lanparty_id = $scope.partyId;
+		$scope.attendeeData.has_attended = true;
 
 		$http.post('/lanparty/' + $scope.partyId + '/admin/roster/add', $scope.attendeeData)
 			.success(function() {
-				$scope.attendees.push(angular.copy($scope.attendeeData));
+				$scope.getAttendees();
 				$scope.attendeeData.firstname = '';
 				$scope.attendeeData.lastname = '';
-				$scope.attendeeData.grad_year = '';
+				$scope.attendeeData.year = '';
 			});
 	};
 
-	$scope.order = function(predicate) {
-		$scope.reverse = !$scope.reverse;
-		$scope.attendees = orderBy($scope.attendees, predicate, $scope.reverse);
+	$scope.toggleAttendance = function(attendeeId) {
+		$http.get('/lanparty/roster/attendee/' + attendeeId + '/toggle').success(function (response) {
+			$scope.getAttendees();
+		});
+	};
+
+	$scope.order = function(sortBy, reverse) {
+		$scope.sortBy = (sortBy === undefined) ? $scope.sortBy : sortBy;
+		$scope.reverse = (reverse === undefined) ? !$scope.reverse : reverse;
+		$scope.attendees = orderBy($scope.attendees, $scope.sortBy, $scope.reverse);
 	};
 
 	$scope.filterAttendees = function(attendee) {
 		if (!$scope.attendeeQuery) return true;
 		var matches = true;
-		var letters = String($scope.attendeeQuery).split('');
-		var stringToMatch = attendee.firstname + attendee.lastname + attendee.grad_year;
+		var letters = String($scope.attendeeQuery.toLowerCase()).split('');
+		var stringToMatch = (attendee.firstname + attendee.lastname + attendee.year).toLowerCase();
 
 		for (var i = 0; i < letters.length; ++i) {
 			if (stringToMatch.indexOf(letters[i]) == -1 && letters[i] != ' ') {
